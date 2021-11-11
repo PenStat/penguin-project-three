3200f76542929584a94cac17efcf5418e8a5d320
JAlbert2 <jonah.albert2@gmail.com>
2021-11-11 14:00:55 -0500

    Redo scaffold

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,10 +1,6 @@
-// dependencies / things imported
 import { LitElement, html, css } from 'lit';
 
-// EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
-// which has the magic life-cycles and developer experience below added
 export class FlashCard extends LitElement {
-  // a convention I enjoy so you can change the tag name in 1 place
   static get tag() {
     return 'flash-card';
   }
@@ -12,55 +8,51 @@ export class FlashCard extends LitElement {
   // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
-    this.need = 'all need to succeed';
-
     setTimeout(() => {
       import('./AnswerBox.js');
+      import('./promptImg.js');
     }, 0);
   }
 
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
-      need: { type: String, reflect: true },
     };
   }
 
   // updated fires every time a property defined above changes
   // this allows you to react to variables changing and use javascript to perform logic
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'need' && this[propName] === 'joy') {
-        this.classList.add('joyful');
-      }
-    });
-  }
+  // updated(changedProperties) {
+  //   changedProperties.forEach((oldValue, propName) => {
+  //   });
+  // }
 
   // Lit life-cycle; this fires the 1st time the element is rendered on the screen
   // this is a sign it is safe to make calls to this.shadowRoot
-  firstUpdated(changedProperties) {
-    if (super.firstUpdated) {
-      super.firstUpdated(changedProperties);
-    }
-  }
+  // firstUpdated(changedProperties) {
+  //   if (super.firstUpdated) {
+  //     super.firstUpdated(changedProperties);
+  //   }
+  // }
 
   // HTMLElement life-cycle, element has been connected to the page / added or moved
   // this fires EVERY time the element is moved
-  connectedCallback() {
-    super.connectedCallback();
-  }
+  // connectedCallback() {
+  //   super.connectedCallback();
+  // }
 
   // HTMLElement life-cycle, element has been removed from the page OR moved
   // this fires every time the element moves
-  disconnectedCallback() {
-    super.disconnectedCallback();
-  }
+  // disconnectedCallback() {
+  //   super.disconnectedCallback();
+  // }
 
   // CSS - specific to Lit
   static get styles() {
     return css`
       :host {
         display: block;
+        outline: black;
       }
     `;
   }
@@ -68,9 +60,9 @@ export class FlashCard extends LitElement {
   // HTML - specific to Lit
   render() {
     return html`
-      <h1>Make me awesome</h1>
-      <p>Build the future we ${this.need}.</p>
-      <slot></slot>
+      <image-prompt><slot name='back'></slot></image-prompt>
+      <p><slot name='front'></slot></p>
+      <answer-box><slot name='back'></slot></answer-box>
     `;
   }
 
