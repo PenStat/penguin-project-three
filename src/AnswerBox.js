bee8781b70286a18c02e66b6d4de465d61eb4837
JAlbert2 <jonah.albert2@gmail.com>
2021-11-18 14:24:05 -0500

    I messed up

--- src/AnswerBox.js
+++ src/AnswerBox.js
@@ -184,7 +184,7 @@ export class AnswerBox extends I18NMixin(SimpleColors) {
       }
       p {
         font-family: Helvetica;
-        color: var(--simple-colors-default-theme-accent-8);
+        color: var(--simple-colors-default-theme-grey-12);
         font-weight: normal;
         font-size: 20px;
       }
